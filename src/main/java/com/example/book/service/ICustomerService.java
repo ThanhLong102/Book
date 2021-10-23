164dfc57bab7fdcc65e36a1d2af790da58b575a9
ThanhLong102 <75972170+ThanhLong102@users.noreply.github.com>
2021-10-23 22:52:36 +0700

    Update ICustomerService.java

--- src/main/java/com/example/book/service/ICustomerService.java
+++ src/main/java/com/example/book/service/ICustomerService.java
@@ -5,7 +5,7 @@ import com.example.book.dto.CustomerDto;
 import java.util.List;
 
 public interface ICustomerService {
-    List<CustomerDto> display();
+    List<CustomerEntity> display();
 
     CustomerDto update(CustomerDto customerDto);
 }
